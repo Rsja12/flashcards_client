d10c50c81ec9c0cae23be013078c986753e81a7b
Rsja12 <asocarras10@gmail.com>
2020-01-13 12:22:07 -0500

    loads js files in html file

--- src/components/topics.js
+++ src/components/topics.js
@@ -2,13 +2,11 @@ class Topics {
     constructor() {
         this.topics = []
         this.adapter = new TopicsAdapter()
-        this.bindEventListeners()
         this.fetchAndLoadTopics()
     }
 
     fetchAndLoadTopics() {
-        this.adapter.getTopics()
-        .then( topics => {
+        this.adapter.getTopics().then( topics => {
             console.log( topics )
         } )
     }

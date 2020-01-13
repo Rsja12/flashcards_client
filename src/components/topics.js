5884506fefc34ba5e06c19f5c5b2232d06e5850c
Rsja12 <asocarras10@gmail.com>
2020-01-13 12:17:57 -0500

    creates fetchAndLoadTopics function

--- src/components/topics.js
+++ src/components/topics.js
@@ -1,7 +1,15 @@
-class Topics() {
+class Topics {
     constructor() {
         this.topics = []
         this.adapter = new TopicsAdapter()
         this.bindEventListeners()
+        this.fetchAndLoadTopics()
+    }
+
+    fetchAndLoadTopics() {
+        this.adapter.getTopics()
+        .then( topics => {
+            console.log( topics )
+        } )
     }
 }

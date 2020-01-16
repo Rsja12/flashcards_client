b63347b6befbf2e8117dbdb02e7c417b2d444cc0
Rsja12 <asocarras10@gmail.com>
2020-01-16 10:42:18 -0500

    adds cards property to Topic class

--- src/components/topic.js
+++ src/components/topic.js
@@ -2,11 +2,12 @@ class Topic {
     constructor(topic) {
         this.id = topic.id
         this.name = topic.name
+        this.cards = topic.cards 
     }
 
     renderTopicName() {
         return `<li data-id=${this.id}></i>${this.name}</li>`
     }
 
-    
+
 }

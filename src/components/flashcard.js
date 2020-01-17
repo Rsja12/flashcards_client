98b3b51a474cbbe5a2b53d7d8bd816e55af59ca2
Rsja12 <asocarras10@gmail.com>
2020-01-17 10:24:35 -0500

    creates flashcard class

---
+++ src/components/flashcard.js
@@ -0,0 +1,8 @@
+class Flashcard {
+    constructor(flashcard) {
+        this.id = flashcard.id 
+        this.name = flashcard.name
+        this.description = flashcard.description
+        this.topic_id = flashcard.topic_id
+    }
+}

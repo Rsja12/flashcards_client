4c79d40c658477c80f72dc465f5b83b473a42502
Rsja12 <asocarras10@gmail.com>
2020-04-14 18:51:43 -0400

    update renderTopicName function

--- src/components/topic.js
+++ src/components/topic.js
@@ -5,8 +5,17 @@ class Topic {
         this.flashcards = topic.flashcards ? topic.flashcards : []
     }
 
+    // renderTopicName() {
+    //     return `<div id="topic-list" class="topic-list" data-id=${this.id}>${this.name}</div>`
+    // }
+
     renderTopicName() {
-        return `<div id="topic-list" class="topic-list" data-id=${this.id}>${this.name}</div>`
+        return `
+            <div data-id=${this.id}>
+                <h4 class="topic-list" data-id=${this.id}>${this.name}</h4>
+                <button class="topic-btn" data-id=${this.id} id="topic-btn">Show Cards</button>
+            </div>
+        `
     }
 
 }
